3fa15a9f5e19d24b275be4eb1e6e94bc2b4ec021
Ashutosh <ashuhina.123@gmail.com>
2024-02-20 22:47:39 +0530

    home

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+/*
+  1. Create a simple web app that displays food recipes
+  2. users should be able to login and search for food recipes based om name or the ingredients used.
+  3. Once the user selects one of the recipes, transition to a new page hwere you will display images, description, ingredients and the steps related to the recipe.
+  4. additionaly a user should be able to sign up, login and add/edit his/her own recipes.
+*/
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,15 @@
+
+const Home = () => {
+  return (
+    <div className="border-solid border-2 border-black h-[calc(100vh-72px)] ">
+        <section className=" border-solid border-2 border-green-500 bg-[url('https://images.unsplash.com/photo-1598214886806-c87b84b7078b?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Zm9vZCUyMGltYWdlc3xlbnwwfHwwfHx8MA%3D%3D'),_url('https://assets.codepen.io/308367/f47b1-bandeirinha-azul-e-branca-png.png')] bg-center bg-[length:50%] bg-no-repeat m-auto w-full h-80" >
+            <h3>Hello</h3>
+        </section>
+        <section>
+            <img src="https://images.unsplash.com/photo-1598214886806-c87b84b7078b?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Zm9vZCUyMGltYWdlc3xlbnwwfHwwfHx8MA%3D%3D'),_url('https://assets.codepen.io/308367/f47b1-bandeirinha-azul-e-branca-png.png" alt="" />
+        </section>
+    </div>
+  )
+}
+
+export default Home
